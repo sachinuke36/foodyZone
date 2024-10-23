4e14c9c6173f0f343ceafbbf807af820717b137a
sachinuke36 <sachinuke36@gmail.com>
2024-10-24 02:09:33 +0530

    fixed admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,42 +5,49 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
-import getSalesData from "./_backend/getSalesData"
+} from "@/components/ui/card";
+import getSalesData from "./_backend/getSalesData";
 import { formatCurrency, formatNumber } from "@/lib/formatter";
 import getUserData from "./_backend/getUserData";
 import getProductData from "./_backend/getFoodData";
-// import { auth } from '@clerk/nextjs/server'
-// import { redirect } from 'next/navigation'
-// import { checkRole } from "@/utils/roles";
 
-
-
-const AdminDashboard = async() => {
-    // console.log(auth())
-    // if (!checkRole('admin')) {
-    //     redirect('/')
-    //   }
-  const [salesData, userData,productData ] = await Promise.all([getSalesData(), getUserData(), getProductData()])
+const AdminDashboard = async () => {
+    const [salesData, userData, productData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+        getProductData(),
+    ]);
 
     return (
         <div className="flex flex-col min-w-full gap-5">
-            <DashboardCard title={'Sales'} description={`${formatNumber(salesData.numberOfSales)} orders`} content={` Rs.${formatNumber(salesData.amount)}`}/>
-            <DashboardCard title={'Customers'} description={`Rs.${formatNumber(userData.averageValuePerUser)} Average Value`} content={` ${formatNumber(userData.userCount)}`}/>
-            <DashboardCard title={'Active Products'} description={`${formatNumber(productData.inActiveCount)} Inactive`} content={`${formatNumber(productData.activeCount)}`} />
-
+            <DashboardCard
+                title={"Sales"}
+                description={`${formatNumber(salesData.numberOfSales)} orders`}
+                content={` Rs.${formatNumber(salesData.amount)}`}
+            />
+            <DashboardCard
+                title={"Customers"}
+                description={`Rs.${formatNumber(userData.averageValuePerUser)} Average Value`}
+                content={` ${formatNumber(userData.userCount)}`}
+            />
+            <DashboardCard
+                title={"Active Products"}
+                description={`${formatNumber(productData.inActiveCount)} Inactive`}
+                content={`${formatNumber(productData.activeCount)}`}
+            />
         </div>
-    )
-}
+    );
+};
 
+export default AdminDashboard;
 
-export default AdminDashboard
+type DashboardCardProps = {
+    title: string;
+    description: string;
+    content: string;
+};
 
-type DashboardCardProps ={
-    title:String, description:String, content:String
-}
-
-export const DashboardCard = ({title, description, content}:DashboardCardProps) => {
+export const DashboardCard = ({ title, description, content }: DashboardCardProps) => {
     return (
         <Card>
             <CardHeader>
@@ -48,9 +55,8 @@ export const DashboardCard = ({title, description, content}:DashboardCardProps) 
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
             <CardContent>
-                <p> {content}</p>
+                <p>{content}</p>
             </CardContent>
         </Card>
-    )
-
-}
+    );
+};
