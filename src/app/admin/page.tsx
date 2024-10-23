c7437d24488f2f71b7248865d2d40053065df171
sachinuke36 <sachinuke36@gmail.com>
2024-10-24 02:06:04 +0530

    fixed admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -27,7 +27,7 @@ const AdminDashboard = async() => {
         <div className="flex flex-col min-w-full gap-5">
             <DashboardCard title={'Sales'} description={`${formatNumber(salesData.numberOfSales)} orders`} content={` Rs.${formatNumber(salesData.amount)}`}/>
             <DashboardCard title={'Customers'} description={`Rs.${formatNumber(userData.averageValuePerUser)} Average Value`} content={` ${formatNumber(userData.userCount)}`}/>
-            <DashboardCard title={'Active Products'} description={`${formatNumber(productData.inActiveCount)} Inactive`} content={formatNumber(productData.activeCount)} />
+            <DashboardCard title={'Active Products'} description={`${formatNumber(productData.inActiveCount)} Inactive`} content={`${formatNumber(productData.activeCount)}`} />
 
         </div>
     )
